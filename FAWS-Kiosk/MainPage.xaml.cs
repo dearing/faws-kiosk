be33576a6d6e536359a66ff9d4bfe7c481e6c008
Jacob Dearing <jacob.dearing@gmail.com>
2016-11-17 01:53:55 -0600

    updates to meta project stuff

--- FAWS-Kiosk/MainPage.xaml.cs
+++ FAWS-Kiosk/MainPage.xaml.cs
@@ -1,29 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-using Windows.System.Threading;
-using System.Threading.Tasks;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace FAWS_Kiosk
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
 
@@ -62,7 +44,6 @@ namespace FAWS_Kiosk
 
         private void Marquee_Loaded(object sender, RoutedEventArgs e)
         {
-            // should trigger Update if successful.
             if (!ApplicationView.GetForCurrentView().TryEnterFullScreenMode())
             {
                 Update(Registry[Index]);
